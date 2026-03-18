3e63c91917dabf4706e593cd34e5bd40f663904a
Adrien <adrien.gosset@epitech.eu>
2026-03-18 12:39:01 +0100

    feat: PLC simulator with register table and read_register

--- crates/simulator/src/lib.rs
+++ crates/simulator/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+use scada_core::{ScadaError, ScadaResult};
+
+pub struct PlcSimulator {
+
+    pub registers: HashMap<u16, u16>,
+    pub device_id: u8 
+}
+
+impl PlcSimulator{
+    pub fn new(device_id:u8) -> Self {
+        let mut registers = HashMap::new();
+        registers.insert(40001, 850);
+        registers.insert(40002, 10 );
+        registers.insert(40003, 250);
+
+        Self { device_id, registers }
+
+    }
+
+    pub fn read_register(&self, address: u16) -> ScadaResult<u16> {
+        match self.registers.get(&address){
+            Some(value) => Ok(*value),
+            None => Err(ScadaError::InvalidAddress(address)),
+        }
+    }
+
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
+    
+    #[test]
+    fn read_existing_registe(){
+        let sim = PlcSimulator::new(1);
+        let result = sim.read_register(40001);
+        assert_eq!(result.unwrap(), 850);
+    }
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn read_invalid_register(){
+        let sim = PlcSimulator::new(2);
+        let result = sim.read_register(65535);
+        assert!(result.is_err());
     }
 }
