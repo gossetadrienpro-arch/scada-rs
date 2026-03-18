100e5b1efa4bdc0e45d48843efe0158b9cef06b6
Adrien <adrien.gosset@epitech.eu>
2026-03-18 13:54:58 +0100

    feat: simulator process_request wires parser to register table

--- crates/simulator/src/lib.rs
+++ crates/simulator/src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use scada_core::{ScadaError, ScadaResult};
+use modbus::{ModbusFrame};
 
 pub struct PlcSimulator {
 
@@ -25,11 +26,22 @@ impl PlcSimulator{
         }
     }
 
+    pub fn process_request(&self, frame: &ModbusFrame) -> ScadaResult<u16> {
+    if frame.data.len() < 2 {
+        return Err(ScadaError::ParseError("data trop court".to_string()));
+    }
+
+    let address = ((frame.data[0] as u16) << 8) | (frame.data[1] as u16);
+
+    self.read_register(address)
+}
+
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use modbus::parse_frame;
     
     #[test]
     fn read_existing_registe(){
@@ -43,5 +55,23 @@ mod test {
         let sim = PlcSimulator::new(2);
         let result = sim.read_register(65535);
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn process_valid_request(){
+        let raw: &[u8] = &[
+
+            0x00, 0x01, //transaction_id = 1
+            0x00, 0x00, // protocol_id = 0
+            0x01, 0x06, // length = 6
+            0x01,       // unit_id = 1
+            0x03,       // function_code =3 
+            0x9c, 0x41,  // adresse 40001
+            0x00, 0x01, // nombre de registres = 1
+        ];
+        let frame = parse_frame(raw).unwrap();
+        let sim = PlcSimulator::new(1);
+        let result = sim.process_request(&frame);
+        assert_eq!(result.unwrap(), 850);
     }
 }
